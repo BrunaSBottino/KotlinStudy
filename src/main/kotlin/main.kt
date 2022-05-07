5c73987bc648cc827e058952be61018bbfd4682c
brunasbottino <brunasbottino@gmail.com>
2022-05-07 17:24:28 -0300

    Few modifications

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,6 +2,38 @@ import kotlin.system.exitProcess
 
 fun main() {
     println("Bem vindo ao Bytebank")
+    var contaBruna = Conta()
+    contaBruna.titular = "Bruna"
+    contaBruna.conta = 1000
+    contaBruna.saldo = 200.0
+    println("Conta 1")
+    println("Titular da conta")
+    println(contaBruna.titular)
+    println("Número da conta")
+    println(contaBruna.conta)
+    println("Saldo da conta")
+    println(contaBruna.saldo)
+
+    println()
+    var contaRodrigo = Conta()
+    println("Conta 2")
+    contaRodrigo.titular = "Rodrigo"
+    contaRodrigo.conta = 1001
+    contaRodrigo.saldo = 300.0
+    println("Titular da conta")
+    println(contaRodrigo.titular)
+    println("Número da conta")
+    println(contaRodrigo.conta)
+    println("Saldo da conta")
+    println(contaRodrigo.saldo)
+}
+
+class Conta{
+    var titular = ""
+    var conta = 0
+    var saldo = 0.0
+}
+fun testaLaços(){
     var i = 0
     while (i < 5){
         var titular = "Bruna $i"
@@ -15,7 +47,6 @@ fun main() {
         i++
     }
 }
-
 fun testaCondicoes(saldo: Double) {
     when {
         saldo > 0 -> println("o saldo da sua conta é positivo")
